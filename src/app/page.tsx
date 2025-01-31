005639b7c4859e7bb65b88703ae29dd2c3a3ec09
Vectsense <vectsense@gmail.com>
2025-01-31 21:05:04 +0900

    ページ遷移アニメーション追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import MotionWrapper from "@/components/motionWrapper/motionWrapper";
 import Image from "next/image";
 
 export default function Home() {
   return (
+    <MotionWrapper>
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-murecho)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
@@ -97,5 +99,6 @@ export default function Home() {
         </a>
       </footer>
     </div>
+    </MotionWrapper>
   );
 }
